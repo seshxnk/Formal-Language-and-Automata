91aea843dd891240e4d5c8a32272aa7dc3b844b7
seshank <98504953+seshxnk@users.noreply.github.com>
2023-11-06 14:18:36 +0530

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef __HEADER_H
+#define __HEADER_H
+#include <map>
+#include <iostream>
+#include <set>
+using namespace std;
+
+map<char, set <string>> rule;
+set<char> terminal; //A B C D
+set<char> nonTerminal; //a b c d 
+
+#endif
+
+
